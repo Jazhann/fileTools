e54dc8c1664d2ed912e9a273566f01c574395163
Daniel Rodriguez <danirg@gmail.com>
2021-04-25 18:48:28 +0200

    names replacement

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ var mkdirp = require('mkdirp');
 
 /**
  * Read a file 
- * @param {*} url file url
+ * @param {*} src file source
  */
-function readFile( url ) {
+function readFile( src ) {
     return new Promise( function ( resolve, reject ) {
-        fs.readFile(url, 'utf8', function (err, data) {
+        fs.readFile(src, 'utf8', function (err, data) {
             if ( err )
                 reject( err );
             else
@@ -18,12 +18,12 @@ function readFile( url ) {
 
 /**
  * Write a file
- * @param {*} url file url
+ * @param {*} src file source
  * @param {*} data data to write
  */
-function writeFile( url, data ) {
+function writeFile( src, data ) {
     return new Promise( function( resolve, reject ) {
-      fs.writeFile( url, data, 'utf8', function(err ) {
+      fs.writeFile( src, data, 'utf8', function(err ) {
         if ( err )
           reject( err );
         else
@@ -35,32 +35,32 @@ function writeFile( url, data ) {
 
 /**
  * Search and modify two differents strings from a file for to differents strings
- * @param {*} url file url
+ * @param {*} src file source
  * @param {*} stringsToReplace array with strings to replace format [["string to replace","replacing string" ]]
  */
-async function modifyFile (url, stringsToReplace ) {
-    if (fs.existsSync(url) && Array.isArray(stringsToReplace)) {
-        console.log( 'Modifying ' + url ); 
-        let data = await readFile( url );
+async function modifyFile (src, stringsToReplace ) {
+    if (fs.existsSync(src) && Array.isArray(stringsToReplace)) {
+        console.log( 'Modifying ' + src ); 
+        let data = await readFile( src );
         data = data.toString();
         stringsToReplace.forEach( element => {
 
             var reg = new RegExp( element[0], "gm" );
             data = data.replace( reg, element[1] );            
         })
-        await writeFile( url, data );
+        await writeFile( src, data );
     } else {
-        console.log( url + ' not found. Skipping' );
+        console.log( src + ' not found. Skipping' );
     }
 }
 
 /**
  * Copy a file 
- * @param {*} source url file 
- * @param {*} target url file
+ * @param {*} src source file 
+ * @param {*} target target file
  */
-function copyFile(source, target, createParentDir = true) {
-   if (fs.existsSync(source)) {
+function copyFile(src, target, createParentDir = true) {
+   if (fs.existsSync(src)) {
       var getDirName = require('path').dirname;
       var destDir = getDirName(target);
       if (!fs.existsSync(destDir)) {
@@ -68,46 +68,46 @@ function copyFile(source, target, createParentDir = true) {
             console.log('Creating directory ' + destDir);
             mkdirp.sync(destDir);
          } else {
-            console.log('Parent directory ' + destDir + ' does NOT exist and not required to create it. Skipping to copy file ' + source + ' to ' + target);
+            console.log('Parent directory ' + destDir + ' does NOT exist and not required to create it. Skipping to copy file ' + src + ' to ' + target);
             return;
          }
 
       }
-      console.log('Copying ' + source + ' to ' + target);
-      fs.createReadStream(source).pipe(fs.createWriteStream(target));
+      console.log('Copying ' + src + ' to ' + target);
+      fs.createReadStream(src).pipe(fs.createWriteStream(target));
    } else {
-      console.log(source + ' not found. Skipping');
+      console.log(src + ' not found. Skipping');
    }
 }
 
 /**
  * Copy multiples files form a directory to another one
- * @param {*} source url directory
- * @param {*} target url directory
+ * @param {*} src source directory
+ * @param {*} target target directory
  */
-function copyFiles( source, target, recursive=true ) {
+function copyRoute( src, target, recursive=true ) {
     var files = [];
-    if ( fs.existsSync( source ) && fs.existsSync( target )) {
-        files = fs.readdirSync( source );
+    if ( fs.existsSync( src ) && fs.existsSync( target )) {
+        files = fs.readdirSync( src );
         files.forEach( function ( file ) {
-            var fileSource = source + '/' + file;
+            var fileSrc = src + '/' + file;
             var fileDest = target + '/' + file ;
-            if (fs.lstatSync(fileSource).isDirectory() && recursive) {
+            if (fs.lstatSync(fileSrc).isDirectory() && recursive) {
                if (!fs.existsSync( fileDest )) {
                   console.log( 'Creating directory ' + fileDest );
                   fs.mkdirSync(fileDest);
                }
-               copyFiles(fileSource, fileDest);
+               copyFiles(fileSrc, fileDest);
             } else {               
-               console.log( 'Copying ' + fileSource + ' to ' + fileDest );
-               fs.createReadStream(fileSource).pipe(fs.createWriteStream (fileDest));
+               console.log( 'Copying ' + fileSrc + ' to ' + fileDest );
+               fs.createReadStream(fileSrc).pipe(fs.createWriteStream (fileDest));
             }
             
         } );
     } else {
-        console.log( source + ' not found. Skipping' );
+        console.log( src + ' not found. Skipping' );
     }
 }
 
 
-module.exports = {  modifyFile, modifyFileWithParams, copyFile, copyFiles };
+module.exports = {  readFile, writeFile, modifyFile, copyFile, copyRoute };
